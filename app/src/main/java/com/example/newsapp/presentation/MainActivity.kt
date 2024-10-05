68e0f0e2630cd5676d7de53c8fd391a71e416527
nice <@https://github.com/mahmoudkarieem>
2024-10-05 19:53:38 +0300

    create splash screen

--- app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.newsapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.newsapp.presentation.theme.NewsAppTheme
 
@@ -18,10 +14,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsAppTheme {
 
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Text("Welcome to the News App!")
-                        // Add more UI components here
-                    }
 
             }
         }
