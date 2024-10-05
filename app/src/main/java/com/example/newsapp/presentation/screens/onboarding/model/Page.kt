ec325c0feeb1a3349756a429645887ec0b13d894
nice <@https://github.com/mahmoudkarieem>
2024-10-05 22:13:35 +0300

    create model for onboarding screen

---
+++ app/src/main/java/com/example/newsapp/presentation/screens/onboarding/model/Page.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp.presentation.screens.onboarding.model
+
+import androidx.annotation.DrawableRes
+import com.example.newsapp.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int
+)
+
+
+val pages = listOf(
+
+    Page(
+        title = "The News App is a modern mobile ",
+        description = "The News App is a modern mobile application that delivers up-to-date",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "The News App is a modern mobile ",
+        description = "The News App is a modern mobile application that delivers up-to-date",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "The News App is a modern mobile ",
+        description = "The News App is a modern mobile application that delivers up-to-date",
+        image = R.drawable.onboarding3
+    )
+
+)
