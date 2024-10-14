20a281f4413239369feb69123aefe578b23e1a61
nice <@https://github.com/mahmoudkarieem>
2024-10-14 14:27:34 +0300

    create apiNews with retrofit with query parameters

---
+++ app/src/main/java/com/example/newsapp/data/remote/api/ApiNews.kt
@@ -0,0 +1,15 @@
+package com.example.newsapp.data.remote.api
+
+import com.example.newsapp.data.remote.dto.NewsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiNews {
+
+    @GET("everything")
+    fun getNews(
+        @Query("sources") sources:String,
+        @Query("page") page:Int,
+        @Query("apiKey") apiKey:String
+    ):NewsResponse
+}
