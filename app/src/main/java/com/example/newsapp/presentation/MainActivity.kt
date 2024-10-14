b5cc7f7dfd4898c2df0136225ecc31190d775cac
nice <@https://github.com/mahmoudkarieem>
2024-10-14 14:22:43 +0300

    create data class to convert data from json to data class kotlin

--- app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/presentation/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.newsapp.MainViewModel
 import com.example.newsapp.presentation.screens.navigation.NavGraph
 import com.example.newsapp.presentation.theme.NewsAppTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -28,6 +32,15 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             NewsAppTheme {
+                val isSystemInLDarkMode = isSystemInDarkTheme()
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInLDarkMode
+                    )
+
+                }
                 Box(
                     modifier = Modifier.background
                         (color = MaterialTheme.colorScheme.background)
--- app/src/main/java/com/example/newsapp/util.kt
+++ app/src/main/java/com/example/newsapp/util.kt
@@ -3,4 +3,5 @@ package com.example.newsapp
 object Utils {
   const   val USER_SETTINGS="userSettings"
     const   val APP_ENTRY="appEntry"
+  const val API_KEY="6f064ac77ac34c30a8211766a59272da"
 }
